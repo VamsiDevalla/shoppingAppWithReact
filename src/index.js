e5213548d5687de1f58566a7ec642ce996611745
Vamsi Devalla <vammichanti40312@gmail.com>
2018-04-19 16:40:55 -0400

    added not found component and implimented routing in index.js file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter,Route,Switch} from 'react-router-dom'
+
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import StorePicker from "./components/StorePicker/StorePicker";
+import NotFound from "./components/NotFound/NotFound"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = ()=>{
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path={'/'} component={StorePicker}/>
+                <Route path={'/store/:storeid'} component={App}/>
+                <Route component={NotFound}/>
+            </Switch>
+
+        </BrowserRouter>
+    )
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
